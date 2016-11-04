a4a26757b3b72a990002c2741565bd0dc3a99890
lucasdemaatdeltion <97045549@st.deltion.nl>
2016-11-04 10:40:26 +0100

    Pinball met code

---
+++ P1/Pinball met code/Assets/Scripts/Bumper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bumper : MonoBehaviour {
+
+    public Rigidbody pinball;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject.Find("GameManager").GetComponent<GameManager>().score += 10;
+    }
+}
---
+++ P1/Pinball met code/Assets/Scripts/Launcher.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launcher : MonoBehaviour {
+
+    public float power;
+    public GameObject prefab;
+    public GameObject spawnLocation;
+    public bool spawnBall;
+
+
+    void Update()
+    {
+        if (spawnBall == false)
+        {
+            if (Input.GetButtonDown("Jump"))
+                {
+                    SpawnPinball();
+                    spawnBall = true;
+                }
+        }
+    }
+
+	void SpawnPinball()
+    {
+        GameObject pinball = (GameObject)(Instantiate(prefab, spawnLocation.GetComponent<Transform>().position, spawnLocation.GetComponent<Transform>().rotation));
+        pinball.GetComponent<Rigidbody>().velocity = -transform.forward * power;
+    }
+
+    
+
+	}
+
