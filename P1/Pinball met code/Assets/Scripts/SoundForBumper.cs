897cd5732ef2a9efb632e745f9e15ca9f20fe607
lucasdemaatdeltion <97045549@st.deltion.nl>
2016-11-07 00:57:59 +0100

    pinball machine star wars

---
+++ P1/Pinball met code/Assets/Scripts/SoundForBumper.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundForBumper : MonoBehaviour {
+    AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Debug-draw all contact points and normals
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+
+        // Play a sound if the colliding objects had a big impact.		
+        if (collision.relativeVelocity.magnitude > 2)
+            audio.Play();
+
+    }
+}
