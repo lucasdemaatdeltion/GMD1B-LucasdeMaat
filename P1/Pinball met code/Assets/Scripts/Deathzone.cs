5a8a1797c25f4c37b4bccb9312660f81fa21f7a1
lucasdemaatdeltion <97045549@st.deltion.nl>
2016-11-07 17:16:05 +0100

    Pinball machine compleet

--- P1/Pinball met code/Assets/Scripts/Deathzone.cs
+++ P1/Pinball met code/Assets/Scripts/Deathzone.cs
@@ -3,22 +3,23 @@ using System.Collections;
 
 public class Deathzone : MonoBehaviour
 {
+    
     // Dit zegt wanneer je iets aan raakt en wat er dan gebeurt. 
     // Hiermee zegt de deathzone dat de pinball weg moet en er één leven af gaat. 
     // Het zegt ook dat de extra ball verdwijnt.
-    void OnCollisionEnter(Collision a)
+    void OnCollisionEnter(Collision col)
     {
-        if (a.gameObject.tag == "Pinball")
+        if (col.gameObject.tag == "Pinball")
         {
             GameObject.Find("GameManager").GetComponent<GameManager>().lives -= 1;
             GameObject.Find("Flipperkast op scholl compleet").GetComponent<Launcher>().spawnBall = false;
         }
-        else if (a.gameObject.tag == "Extraball")
+        else if (col.gameObject.tag == "Extraball")
         {
 
         }
 
-        Destroy(a.gameObject);
+        Destroy(col.gameObject);
     }
 
 }
